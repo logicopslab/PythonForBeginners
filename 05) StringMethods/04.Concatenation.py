9367f967e215990691acf5ac58e75196177a9f37
logicopslab <logicopslab@gmail.com>
2021-11-02 19:47:25 +0530

    Concatenation

--- 05) StringMethods/04.Concatenation.py
+++ 05) StringMethods/04.Concatenation.py
@@ -11,3 +11,17 @@ print(':D '*10)
 
 r_s = "Yasss! " * 4
 print(r_s)
+
+# String Join
+# It is a string method and returns 
+# a string in which the elements of the sequence have been 
+# joined by the str separator.
+
+str = ""   # string  
+list = ['L','o','g','i','c','O','p','s',' ','L','a','b']    
+  
+# Calling function  
+str2 = str.join(list)  
+# Displaying result  
+print(str2)  
+
