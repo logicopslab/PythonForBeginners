64f70dc078646caadbc1108b64ff42fc60bee69f
logicopslab <logicopslab@gmail.com>
2021-11-02 19:01:24 +0530

    Introducing Strings Methods

---
+++ 05) StringMethods/02.StringMethods copy.py
@@ -0,0 +1,20 @@
+
+# initializing target string
+sentence = "lab_logicopslab"
+
+# initializing argument string
+arg_string = "lab"
+
+# using index() to find position of "logic"
+# starting from various indexes
+
+position = sentence.index(arg_string,0)
+print ("The first position of logic after 0th index : ",end="")
+print (position)
+
+position = sentence.index(arg_string,5)
+print ("The first position of logic after 5th index : ",end="")
+
+print (position)
+
+
