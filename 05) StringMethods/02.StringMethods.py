6df318a656ac8954c67ce8778dfff0b1ff464c2d
logicopslab <logicopslab@gmail.com>
2021-11-03 00:57:08 +0530

    Final

--- 05) StringMethods/02.StringMethods.py
+++ 05) StringMethods/02.StringMethods.py
@@ -9,11 +9,11 @@ arg_string = "lab"
 # starting from various indexes
 
 position = sentence.index(arg_string,0)
-print ("The first position of logic after 0th index : ",end="")
+print ("The first position of lab after 0th index : ",end="")
 print (position)
 
 position = sentence.index(arg_string,5)
-print ("The first position of logic after 5th index : ",end="")
+print ("The first position of lab after 5th index : ",end="")
 
 print (position)
 
